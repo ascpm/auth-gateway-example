2e8d81a3c29093ec27c9327ff3b633f749951923
ascpm <ascpm01@gmail.com>
2019-06-28 15:56:21 +0900

    docker-run.sh file updated (8080 port disabled)

--- docker-run.sh
+++ docker-run.sh
@@ -7,5 +7,5 @@ group=(`cat gradle.properties | grep "group" | cut -d= -f2`)
 name=(`cat gradle.properties | grep "serviceName" | cut -d= -f2`)
 image_tag=(`cat gradle.properties | grep "serviceVersion" | cut -d= -f2`)
 
-docker run -d -p 8080:8080 -p 8100:8100 --name="jwt" $group/$name:"${image_tag}"
+docker run -d -p 8100:8100 --name="jwt" $group/$name:"${image_tag}"
 docker logs -f jwt
